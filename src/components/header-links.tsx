77ba7cbfefba330243c40e71b25accfe1b6d2fba
jmxor <jamesmrippeth@gmail.com>
2024-11-28 18:00:17 +0000

    Extract header links and header search into individual components

---
+++ src/components/header-links.tsx
@@ -0,0 +1,31 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+export interface NavLink {
+  label: string;
+  url: string;
+}
+
+export function HeaderLinks({ links }: { links: NavLink[] }) {
+  return (
+    <NavigationMenu className="hidden lg:block">
+      <NavigationMenuList>
+        {links.map((link) => (
+          <NavigationMenuItem key={link.url}>
+            <Link href={link.url} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {link.label}
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,7 @@
+import { HeaderLinks, NavLink } from "@/components/header-links";
+import HeaderSearch from "@/components/header-search";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 import Link from "next/link";
-
-interface NavLink {
-  label: string;
-  url: string;
-}
 
 const navLinks: NavLink[] = [
   { label: "View Tools", url: "/tools" },
@@ -24,32 +13,12 @@ export default function Header() {
     <header className="flex h-16 w-full items-center border-b px-4">
       <div className="mr-auto">Tool Share</div>
 
-      {/* Desktop Links*/}
-      <NavigationMenu className="mr-4 hidden lg:block">
-        <NavigationMenuList>
-          {navLinks.map((link) => (
-            <NavigationMenuItem key={link.url}>
-              <Link href={link.url} legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  {link.label}
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
-
       <div className="flex items-center gap-4">
-        <div className="flex rounded-md has-[:focus-visible]:ring-1 has-[:focus-visible]:ring-ring">
-          <Input
-            type="search"
-            placeholder="Search tools..."
-            className="rounded-r-none border-r-0 focus-visible:ring-0"
-          />
-          <Button type="submit" variant="outline" className="rounded-l-none">
-            Go
-          </Button>
-        </div>
+        {/* Desktop Links*/}
+        <HeaderLinks links={navLinks} />
+
+        <HeaderSearch />
+
         <Button variant="outline" asChild>
           <Link href="/auth/login">Login</Link>
         </Button>
