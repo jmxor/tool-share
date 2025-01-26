49967882e19555e6991cbeb7be66e58ad8ac1e6f
Tomas Meneses <tomasmeneses2012@gmail.com>
2025-01-26 17:49:57 +0000

    Created all remaining tables in the database and added queries to tables.sql

--- tables.sql
+++ tables.sql
@@ -54,3 +54,110 @@ CREATE TABLE IF NOT EXISTS post_category (
     FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS transaction (
+    id SERIAL PRIMARY KEY,
+    post_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    borrowed_at TIMESTAMP,
+    returned_at TIMESTAMP,
+    transaction_status VARCHAR(64) NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    completed_at TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS borrow_request (
+    id SERIAL PRIMARY KEY,
+    requester_id INT,
+    post_id INT NOT NULl,
+    requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    requested_length INTERVAL NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    result BOOLEAN,
+    FOREIGN KEY (requester_id) REFERENCES "user" (id) ON DELETE SET NULL,
+    FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS tool_request (
+    id SERIAL PRIMARY KEY,
+    tool_name VARCHAR(64),
+    category_id INT,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULl,
+    status VARCHAR(64) NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE SET NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS post_picture (
+    id SERIAL PRIMARY KEY,
+    post_id INT NOT NULl,
+    source VARCHAR(256),
+    FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS review (
+    id SERIAL PRIMARY KEY,
+    reviewer_id INT NOT NULl,
+    reviewed_id INT NOT NULL,
+    stars INT NOT NULl,
+    review_text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULl,
+    FOREIGN KEY (reviewer_id) REFERENCES "user" (id) ON DELETE CASCADE,
+    FOREIGN KEY (reviewed_id) REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS suspension (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULl,
+    admin_id INT,
+    starts_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULl,
+    expires_at TIMESTAMP NOT NULL,
+    reason TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES "user" (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS transaction_code (
+    id SERIAL PRIMARY KEY,
+    transaction_id INT NOT NULL,
+    active BOOLEAN NOT NULL,
+    code INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULl,
+    expires_at TIMESTAMP NOT NULL,
+    used_at TIMESTAMP,
+    FOREIGN KEY (transaction_id) REFERENCES transaction (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS extension_request (
+    id SERIAL PRIMARY KEY,
+    transaction_id INT NOT NULL,
+    extension_length INTERVAL NOT NULL,
+    new_expiration TIMESTAMP NOT NULL,
+    request_status VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (transaction_id) REFERENCES transaction (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS report (
+    id SERIAL PRIMARY KEY,
+    accuser_id INT,
+    accused_id INT,
+    transaction_id INT NOT NULl,
+    report_description TEXT NOT NULL,
+    report_status VARCHAR(64),
+    FOREIGN KEY (accuser_id) REFERENCES "user" (id) ON DELETE SET NULL,
+    FOREIGN KEY (accused_id) REFERENCES "user" (id) ON DELETE SET NULL,
+    FOREIGN KEY (transaction_id) REFERENCES transaction (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS report_message (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    report_id INT NOT NULl,
+    message TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE SET NULL,
+    FOREIGN KEY (report_id) REFERENCES report (id) ON DELETE CASCADE
+);
