3b58dcc254edbfd64a5a650e7dd428938d105532
jmxor <jamesmrippeth@gmail.com>
2025-02-02 13:26:10 +0000

    Condense spacing between input fields

--- src/app/tools/new/page.tsx
+++ src/app/tools/new/page.tsx
@@ -57,12 +57,12 @@ export default function NewToolPage() {
           <h2 className="text-center text-3xl font-bold text-gray-800">
             Share a new Tool
           </h2>
-          <div className="space-y-4">
+          <div className="space-y-2">
             <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="min-h-[84px]">
                   <FormLabel>Name*</FormLabel>
                   <FormControl>
                     <Input {...field} />
@@ -76,7 +76,7 @@ export default function NewToolPage() {
               control={form.control}
               name="description"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="min-h-[108px]">
                   <FormLabel>Description*</FormLabel>
                   <FormControl>
                     <Textarea {...field} />
@@ -86,39 +86,41 @@ export default function NewToolPage() {
               )}
             />
 
-            <FormField
-              control={form.control}
-              name="deposit"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Deposit*</FormLabel>
-                  <FormControl>
-                    <Input {...field} type="number" step="0.01" />
-                  </FormControl>
-                  <FormMessage>{state.errors?.deposit}</FormMessage>
-                </FormItem>
-              )}
-            />
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="deposit"
+                render={({ field }) => (
+                  <FormItem className="min-h-[84px]">
+                    <FormLabel>Deposit*</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="number" step="0.01" />
+                    </FormControl>
+                    <FormMessage>{state.errors?.deposit}</FormMessage>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="max_borrow_days"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Max Borrow Period*</FormLabel>
-                  <FormControl>
-                    <Input {...field} type="number" />
-                  </FormControl>
-                  <FormMessage>{state.errors?.max_borrow_days}</FormMessage>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="max_borrow_days"
+                render={({ field }) => (
+                  <FormItem className="min-h-[84px]">
+                    <FormLabel>Max Borrow Period*</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="number" />
+                    </FormControl>
+                    <FormMessage>{state.errors?.max_borrow_days}</FormMessage>
+                  </FormItem>
+                )}
+              />
+            </div>
 
             <FormField
               control={form.control}
               name="location"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="min-h-[84px]">
                   <FormLabel>Location*</FormLabel>
                   <FormControl>
                     <Input {...field} />
@@ -132,7 +134,7 @@ export default function NewToolPage() {
               control={form.control}
               name="images"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="min-h-[84px]">
                   <FormLabel>Images*</FormLabel>
                   <FormControl>
                     <Input type="file" accept="image/*" multiple {...fileRef} />
@@ -147,7 +149,7 @@ export default function NewToolPage() {
               control={form.control}
               name="categories"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="min-h-[84px]">
                   <FormLabel>Categories*</FormLabel>
                   <FormControl>
                     <Input {...field} />
