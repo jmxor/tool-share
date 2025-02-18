5a4f943e0f2a1e0c17f392b73ff718844aed7b64
jmxor <jamesmrippeth@gmail.com>
2025-02-18 15:49:08 +0000

    Add basic shadcn DataTable to render tools information

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,7 +1,33 @@
 import { Button } from "@/components/ui/button";
+import DataTable from "@/components/ui/data-table";
+import { getTools } from "@/lib/actions";
+import { ColumnDef } from "@tanstack/table-core";
 import { Plus } from "lucide-react";
 
-export default function ToolsPage() {
+// TODO: move all types to actions file
+type Tool = {
+  id: number;
+  user_id: number;
+  tool_name: string;
+  description: string;
+  deposit: number;
+  max_borrow_days: number;
+  location_id: number;
+  status: string;
+};
+
+const toolsColumns: ColumnDef<Tool>[] = [
+  { accessorKey: "tool_name", header: "Name" },
+  { accessorKey: "description", header: "Description" },
+  { accessorKey: "deposit", header: "Deposit" },
+  { accessorKey: "max_borrow_days", header: "Max Borrow Time (days)" },
+  { accessorKey: "location_id", header: "Location" },
+  { accessorKey: "status", header: "Status" },
+];
+
+export default async function ToolsPage() {
+  const tools = await getTools();
+
   return (
     <div className="grid grid-cols-1 gap-2 p-2 lg:grid-cols-2 lg:px-4">
       <div className="flex items-center justify-center gap-2 lg:col-span-2">
@@ -20,9 +46,7 @@ export default function ToolsPage() {
         <h1>Map Section</h1>
       </div>
 
-      <div className="col-span-1 flex h-36 items-center justify-center rounded-md border px-4 py-2">
-        <h1>Table Section</h1>
-      </div>
+      <DataTable columns={toolsColumns} data={tools} />
     </div>
   );
 }
