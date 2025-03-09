6a88eb9b4a30c2b6aac5952dcd3cbfa7252ec9b3
Tomas Meneses <tomasmeneses2012@gmail.com>
2025-03-09 23:35:12 +0000

    Fix missing type for build.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,13 +7,13 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
-export function formatDate(dateString) {
-    const date = new Date(dateString);
+export function formatDate(dateIn: Date) {
+    const date = new Date(dateIn);
     const day = date.getDate();
     const month = date.toLocaleString('default', { month: 'long' });
     const year = date.getFullYear();
 
-    const ordinal = (n) =>
+    const ordinal = (n: number) =>
         n + (['st', 'nd', 'rd'][(n % 10) - 1] || 'th');
 
     return `${ordinal(day)} ${month} ${year}`;
