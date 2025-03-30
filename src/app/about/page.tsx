a2b56be4cf8e24af5632ac23f1f26dbe73645460
jmxor <jamesmrippeth@gmail.com>
2025-03-30 16:00:13 +0100

    Add basic transaction steps steps

---
+++ src/app/about/page.tsx
@@ -0,0 +1,21 @@
+export default function AboutPage() {
+  return (
+    <div className="p-4">
+      <h1>How It Works</h1>
+      <ol>
+        <li>1. Click borrow on a tool</li>
+        <li>2. Enter time you would like to borrow the tool for</li>
+        <li>3. Wait for tool owner to accept or counter offer</li>
+        <li>4. Make payment and arrange meeting to handover tool</li>
+        <li>
+          5. Handover: Enter code from owner to confirm identity before handover
+          of tool
+        </li>
+        <li>
+          6. Return: Generate code for owner to enter to confirm return of tool{" "}
+        </li>
+        <li></li>
+      </ol>
+    </div>
+  );
+}
