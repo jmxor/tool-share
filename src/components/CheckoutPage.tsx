80235d059e06b44bc6a27b85c456d6f2b5b3ba3c
Tomas Meneses <tomasmeneses2012@gmail.com>
2025-04-09 21:41:24 +0100

    Changed dollar to gbp symbol

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -87,7 +87,7 @@ const CheckoutPage = ({ amount, transaction_id }: { amount: number, transaction_
         disabled={!stripe || loading}
         className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
       >
-        {!loading ? `Pay $${amount}` : "Processing..."}
+        {!loading ? `Pay £${amount}` : "Processing..."}
       </button>
     </form>
   );
